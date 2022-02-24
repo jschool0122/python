522975142531af0ca96136e4dcf506cb256c2117
jschool0122 <99379568+jschool0122@users.noreply.github.com>
2022-02-24 22:34:37 +0900

    Update 엑셀파일_합치기(2022.02.24.).py

--- 엑셀파일_합치기(2022.02.24.).py
+++ 엑셀파일_합치기(2022.02.24.).py
@@ -16,11 +16,10 @@ df = pd.DataFrame()
 for file in files:
     if file.endswith('.xlsx'):
         df = df.append(pd.read_excel(file, header = 2), ignore_index=True)  #df에 pd.read_excel 값을 추가한다. 단, 헤더는 위에서 3번째 행부터. 인덱스 번호는 통합해서 센다.
-        columns = ['학번', '이름', '내용']
-        df.index = df.index + 1
+        df.index = df.index + 1 #인덱스가 0부터 시작하니까 1부터 시작하도록 변경.
 df.head()
 
-df.iloc[0:20, 1:4]
+df.iloc[0:20, 1:4]#df.loc[행 범위, 열 범위]
 
 
 df.to_excel('통합_엑셀파일.xlsx')
